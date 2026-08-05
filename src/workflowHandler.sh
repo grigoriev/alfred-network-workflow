a4eed33d82e6232c669d87f4d8b99c485cdbad8c
Sergey Grigoriev <grigoriev@fastmail.com>
2026-08-05 14:42:09 +0300

    chore: remove unused getLang helper

--- src/workflowHandler.sh
+++ src/workflowHandler.sh
@@ -110,7 +110,3 @@ getPref() {
   local VALUE=$(sed "/^\#/d" "$PREFFILE" | grep "$1"  | tail -n 1 | cut -d "=" -f2-)
   echo "$VALUE"
 }
-
-getLang() {
-  defaults read .GlobalPreferences AppleLanguages | tr -d [:space:] | cut -c2-3
-}
